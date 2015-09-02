344e18d4d46863df13737e1863c827e5f1af79d6
William Heslam <willheslam@gmail.com>
2015-09-02 18:03:37 +0100

    Make the regexp a bit more efficient and correct

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var stringsAndFlowComments = /(`(?:.|\s)*?`|'(?:.|\s)*?'|`(?:.|\s)*?`|"(?:.|\s)*?"|\/\*.*?(?::{1,2}|flow-include).*?(?:.|\s)*?\*\/)/;
+var stringsAndFlowComments = /(`(?:[^`\\]|\\.)*?`|'(?:[^'\\]|\\.)*?'|"(?:[^"\\]|\\.)*?"|\/\*.*?(?::{1,2}|flow-include).*?[\s\S]*?\*\/)/;
 
 function removeComments(code){
 	return code.split(stringsAndFlowComments)
 	.map( function(chunk){
-		if(chunk.trim().search(/\/\*.*?(?::{1,2}|flow-include).*?(?:.|\s)*?\*\//) === 0){
-			var include = /\/\*.*?(?::{2}|flow-include)(.*?(?:.|\s)*)\*\//;
-			var inline  = /\/\*(.*?:.*?(?:.|\s)*)\*\//;
+		if(chunk.trim().search(/\/\*.*?(?::{1,2}|flow-include).*?[\s\S]*?\*\//) === 0){
+			var include = /\/\*.*?(?::{2}|flow-include)(.*?[\s\S]*)\*\//;
+			var inline  = /\/\*(.*?:.*?[\s\S]*)\*\//;
 			var pattern = include.test(chunk) ? include : inline;
 		
 			var flowInside = pattern.exec(chunk);
